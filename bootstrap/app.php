7625c98d9a0006bdc286945241d5c28ddf99a242
seiyanz16 <zaskiafitrisholehah@gmail.com>
2025-06-01 07:10:53 +0700

    fix: unpublish cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
+        // $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->renderable(function (AuthenticationException $e, $request) {
